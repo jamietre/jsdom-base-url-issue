fcc667020e62ccf689c388ebd74a3f404657e82c
James Treworgy <jamietre@gmail.com>
2016-02-04 11:52:57 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var jsdom = require('jsdom');
+var fs = require('fs');
+
+var app = express();
+app.use("/", express.static(__dirname+'/http/'));
+app.use("/scripts", express.static(__dirname+'/http/styles'));
+
+var server = app.listen(5000);
+
+var defaultDocumentFeatures = {
+    FetchExternalResources   : ['link']
+};
+
+var vcon = jsdom.createVirtualConsole();
+vcon.sendTo(console); 
+
+
+
+var html = fs.readFileSync(__dirname+'/http/index.html', 'utf8');
+
+var window = jsdom.jsdom(html, {
+    virtualConsole: vcon
+}).defaultView;
+
+window.addEventListener('load', function () {
+    var el = window.document.querySelector('p');
+
+    console.log("color: " + window.getComputedStyle(el).fontColor);
+    console.log("weight: " + window.getComputedStyle(el).fontWeight);
+
+});
